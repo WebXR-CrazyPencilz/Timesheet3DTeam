e8ed4b3a09d171f61c3fd91973bc7620b643fadb
WebXR-CrazyPencilz <3dactz@gmail.com>
2026-06-19 17:07:06 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
   // ► STEP 1: Paste your Apps Script Web App URL here
-  SHEETS_URL: 'https://script.google.com/macros/s/AKfycbzmn063QGJNj3K60K3ZOjdR1Sr96CnfJ3PPuDKkdOOKZG2ry0U_XmX68LlGkOwOw2L1/exec',
+  SHEETS_URL: 'https://script.google.com/macros/s/AKfycbwqemYpq0Jt8BYpaCoOAoDxkSDTB-Cz3HstN3FS1WQJL4cJYbU7MQqVpr_lh7bWvmBmrw/exec',
 
   // ► STEP 2: Set to false once your Sheet is connected
   DEMO_MODE: false,
